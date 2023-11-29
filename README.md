d6d67ba8d0378e15ab7f25ea0bcb7566e623fd31
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-11-29 13:41:34 +0100

    integrate pydantic model with uuid and add hidden fields to form buttons to submit uuid to backend
    extend Readme.md

--- README.md
+++ README.md
@@ -13,16 +13,28 @@ Die einzelenen ToDos sollen in einer MYSQL Datenbank gespeichert werden inkl. de
 Erstelle Dir ein Repository in Deinem Github Account und übertrage den Code dorthin.
 Achte darauf, dass Du die Inhalte des VENV nicht mit in Dein Repo überträgst.
 
-## Links 
+## Links
+Wir erstellen eine Server-Anwendung die auf uvicorn basiert.
+Wir erstellen verschiedene Routen die mit FastAPI implementiert werden.
+Wir benutzen Jinja2 als Templating Engine um HTML Dateien aus einem Verzeichnis namens templates zu laden, und dort bestimmte Variablen zu ersetzen bzw. Bedingungen auszuführen.
+Wir benutzen Pydantic als Datenmodell für die Liste der Todo items.
+
+- [Frontend Intro](https://docs.google.com/presentation/d/1YoxRv7m7NmFjPQwffTqlgyJt0wIOvlMiX2XDnBZWjbY) 
 - [Fastapi tutorial](https://fastapi.tiangolo.com/tutorial/)
 - [Jinja2 loops](https://ttl255.com/jinja2-tutorial-part-2-loops-and-conditionals/#loops)
 - [To use forms, first install python-multipart. pip install python-multipart](https://fastapi.tiangolo.com/tutorial/request-forms/)
 - [pydantic data model](https://docs.pydantic.dev/latest/examples/secrets/)
 - [pydantic BaseModel](https://docs.pydantic.dev/latest/concepts/models/)
+- [Pydantic UUID](https://docs.pydantic.dev/2.0/usage/types/uuids/)
+- [UUID Example](https://docs.pydantic.dev/latest/concepts/fields/)
+- [Jinja2 loop.index](https://jinja.palletsprojects.com/en/3.0.x/templates/)
 
-## Benutzung
+## Benutzung (unter GitBash)
 python -m venv env
-danach environment aktivieren source env/Scripts/activate(mit Git Bash)
+
+danach environment aktivieren mittels:
+
+source env/Scripts/activate
 
 pip install -r requirements.txt
 cd src
--- src/main.py
+++ src/main.py
@@ -3,37 +3,45 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import Annotated, List
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4
 import uuid
 
 app = FastAPI()
 
-class ToDoItem(BaseModel):
-    id: int 
+class TodoItem(BaseModel):
+    itemId: UUID4
     description:str
     status: str 
     
+class TodoItemList(list):
+    def append(self, item):
+        if not isinstance(item, TodoItem):
+            raise TypeError("Only ToDoItem instances can be added to the list")
+        super().append(item)
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
-items = List[ToDoItem]
+items = TodoItemList()
+
+
 @app.post("/save", response_class=HTMLResponse)
 async def save_list(request: Request, item:Annotated[str, Form()]):
     print(item)
     myuuid = uuid.uuid4()
     print('Your UUID is: ' + str(myuuid))
-    Aufgabe=ToDoItem(description=item, id=myuuid)
-    items.append(item)
+    task=TodoItem(description=item, itemId=str(myuuid), status="neu") 
+    items.append(task)
     return templates.TemplateResponse("index.html", {"request": request, "items":items})
 
 @app.post("/delete", response_class=HTMLResponse)
-async def delete_route(request: Request):
+async def delete_route(request: Request, itemId:Annotated[str, Form()]):
+    print("Deleting item:", itemId)
     return templates.TemplateResponse("index.html", {"request": request, "items":items})
 
 @app.post("/start", response_class=HTMLResponse)
-async def start_route(request: Request):
+async def start_route(request: Request,itemId:Annotated[str, Form()]):
+    print("Starting item:", itemId)
     return templates.TemplateResponse("index.html", {"request": request, "items":items})
 
 
