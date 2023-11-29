adff52a2a0704b725594341c170993a1e3fc44ef
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-11-29 18:26:29 +0100

    first stab at integrating database.py

--- src/database.py
+++ src/database.py
@@ -1,3 +1,5 @@
+import enum
+
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, Enum
 
@@ -9,16 +11,16 @@ engine = create_engine('mysql+mysqlconnector://todo_user:todo_password@localhost
 
 Base = declarative_base()
 
-class ItemStatus(Enum):
+class ItemStatus(enum.Enum):
     OPEN='open'
     INPROGRESS='in progress'
     FINISHED='finished'
 
-class TodoListItems(Base):
+class TodoListItem(Base):
     __tablename__ = 'items'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     description = Column(String(128))
-    status = Column(String(16))
+    status = Column(Enum(ItemStatus))
 
 Base.metadata.create_all(engine)
 
--- src/main.py
+++ src/main.py
@@ -21,32 +21,71 @@ class ItemStatus(Enum):
     INPROGRESS='in progress'
     FINISHED='finished'
 
-class TodoItem(BaseModel):
+class OldTodoItem(BaseModel):
     itemId: UUID4
     description:str
     status: ItemStatus
-    
-class TodoItemList(list):
+
+class OldTodoItemList(list):
     def append(self, item):
-        if not isinstance(item, TodoItem):
+        if not isinstance(item, TodoListItem):
             raise TypeError("Only ToDoItem instances can be added to the list")
         super().append(item)
     def remove(self, item):
         for myitem in self[:]:
-            if str(myitem.itemId) == item or myitem.itemId == uuid.UUID(item):
+            if str(myitem.itemId) == item:
                 print("Deleting:", item)
                 super().remove(myitem)
-            
-items = TodoItemList()
+
+class TodoItemList(list):
+    def __init__(self, session):
+        self.session = session
+        self.load_initial_data()
+
+    def load_initial_data(self):
+        """Load initial data from the database to sync with the list."""
+        items = self.session.query(database.TodoListItem).all()
+        super().extend(items)
+
+    def append(self, item):
+        if not isinstance(item, database.TodoListItem):
+            raise TypeError("Only TodoItem instances can be added to the list")
+        self.session.add(item)
+        try:
+            self.session.commit()
+            super().append(item)
+        except Exception as e:
+            self.session.rollback()
+            raise e
+
+    def remove(self, item):
+        to_remove = None
+        for myitem in self:
+            if myitem.id == item:
+                to_remove = myitem
+                break
+        if to_remove:
+            self.session.delete(to_remove)
+            try:
+                self.session.commit()
+                super().remove(to_remove)
+            except Exception as e:
+                self.session.rollback()
+                raise e
+        else:
+            raise ValueError("Item not found in the list")
+
+
+items = TodoItemList(database.session)
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.post("/save", response_class=HTMLResponse)
 async def save_list(request: Request, item:Annotated[str, Form()]):
     print(item)
-    myuuid = uuid.uuid4()
-    print('Your UUID is: ' + str(myuuid))
-    task=TodoItem(description=item, itemId=str(myuuid), status=ItemStatus.OPEN) 
+    #myuuid = uuid.uuid4()
+    #print('Your UUID is: ' + str(myuuid))
+    task=database.TodoListItem(description=item, status=database.ItemStatus.OPEN) 
     items.append(task)
     return templates.TemplateResponse("index.html", {"request": request, "items":items})
 
@@ -57,21 +96,21 @@ async def delete_route(request: Request, itemId:Annotated[str, Form()]):
     return templates.TemplateResponse("index.html", {"request": request, "items":items})
 
 @app.post("/start", response_class=HTMLResponse)
-async def start_route(request: Request,itemId:Annotated[str, Form()]):
+async def start_route(request: Request, itemId:Annotated[str, Form()]):
     for item in items:
-        if str(item.itemId) == itemId:
-            if item.status == ItemStatus.OPEN:
-                 item.status = ItemStatus.INPROGRESS
+        if item.id == itemId:
+            if item.status == database.ItemStatus.OPEN:
+                 item.status = database.ItemStatus.INPROGRESS
             break 
     print("Starting item:", itemId)
     return templates.TemplateResponse("index.html", {"request": request, "items":items})
 
 @app.post("/finish", response_class=HTMLResponse)
-async def finish_route(request: Request,itemId:Annotated[str, Form()]):
+async def finish_route(request: Request, itemId:Annotated[str, Form()]):
     for item in items:
-        if str(item.itemId) == itemId:
-            if item.status == ItemStatus.INPROGRESS:
-                 item.status = ItemStatus.FINISHED
+        if item.id == itemId:
+            if item.status == database.ItemStatus.INPROGRESS:
+                 item.status = database.ItemStatus.FINISHED
             break 
     print("Finishing item:", itemId)
     return templates.TemplateResponse("index.html", {"request": request, "items":items})
