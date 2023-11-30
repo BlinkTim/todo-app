6fdb0b9178f08c28ce0469507530ef2b03d4af53
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-11-30 18:35:40 +0100

    add missing venv activation

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ wget https://todolist-bucket-1.s3.eu-central-1.amazonaws.com/todo-app.zip
 unzip todo-app.zip
 # cd todo-app
 python3 -m venv env
+. env/bin/activate
 pip install -r requirements.txt
 sudo mysql -u root -p < sql-scripts/01-prepare-db.sql
 cd src
