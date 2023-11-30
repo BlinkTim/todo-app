687dacbc34b5d486016851db0505aeff46ab8e2b
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-11-30 18:30:15 +0100

    fix up userdata script

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ PMGR=apt
 $PMGR update -y 
 $PMGR install mysql-server -y
 $PMGR install python3 python3-pip -y
-#download zipfile and unzip and cd in the new folder
+# download zipfile and unzip and cd in the new folder
 wget https://todolist-bucket-1.s3.eu-central-1.amazonaws.com/todo-app.zip
 unzip todo-app.zip
-cd todo-app
+# cd todo-app
 python3 -m venv env
 pip install -r requirements.txt
-mysql -u root -p < sql-scripts/01-prepare-db.sql
+sudo mysql -u root -p < sql-scripts/01-prepare-db.sql
 cd src
 uvicorn main:app --reload
